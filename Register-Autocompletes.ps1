8f1c3b2330b4d8276f4223be2cb4316bbfd35191
Sondre071 <sirciller@gmail.com>
2025-01-10 20:39:18 +0100

    Feat: Add register-autocompletes file wip

---
+++ Register-Autocompletes.ps1
@@ -0,0 +1,23 @@
+$Config = ((Get-Content -Path "$PSScriptRoot/config.json") | ConvertFrom-Json).Actions.FunctionGroups
+
+Write-Host $Config
+
+foreach ($ActionType in $Config.PSObject.Properties) {
+
+    Write-host $ActionType.Name
+    Write-host $ActionType.Value
+
+    Register-ArgumentCompleter -CommandName $ActionType.Name -ParameterName Path -ScriptBlock {
+
+        foreach ($Parameter in $Config.$ActionType.Value.Parameters.PSObject.Properties) {
+            New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList @(
+                $Parameter.Name
+                $Parameter.Name
+                'ParameterValue'
+                $Parameter.Name
+            )
+        }
+    }.GetNewClosure()
+}
+
+Remove-Variable -Name Config
