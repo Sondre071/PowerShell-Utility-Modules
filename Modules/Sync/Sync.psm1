d8e52a615f52f5d41113f895969d5f5d04bf2238
Sondre071 <Sirciller@gmail.com>
2025-03-22 00:00:35 +0100

    Cleanup: Remove sync test

--- Modules/Sync/Sync.psm1
+++
@@ -1,9 +0,0 @@
-function Sync() {
-    $UserConfig = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\..\config.json") | ConvertFrom-Json
-}
-
-function RenderMenu() {
-    Write-Host "THIS IS A MENU ...."
-}
-
-Export-ModuleMember Sync
