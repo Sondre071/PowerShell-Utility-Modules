c01530e73232d251670bee1754e97408108a6017
Sondre071 <Sirciller@gmail.com>
2025-03-22 16:10:05 +0100

    Fix spelling

--- Modules/Read-Menu/Read-Menu.psm1
+++ Modules/Read-Menu/Read-Menu.psm1
@@ -3,7 +3,7 @@ function Read-Menu {
         [Parameter(Mandatory = $true)]
         [string[]]$MenuArray,
 
-        # Optional parameter to bypass the menu sort.
+        # Optional parameter to bypass the menu sorting.
         [string]$LastEntry
     )
 
