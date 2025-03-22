d1f689a2bca71c8700b5a73f53ded82841651e29
Sondre071 <Sirciller@gmail.com>
2025-03-22 16:55:33 +0100

    Cleanup: Improve property naming

--- Modules/Edit/Edit.psm1
+++ Modules/Edit/Edit.psm1
@@ -1,19 +1,19 @@
 [PSObject]$Config = (Use-Config).Data.Edit
 
 function Edit($PathKey) {
-    if (-not $Config.Parameters.PSObject.Properties.Length) {
+    if (-not $Config.Paths.PSObject.Properties.Length) {
         Write-Host "No keys found."
         return
     }
 
-    $ConfigKey = if ($PathKey) { $PathKey } else { (Read-Menu -MenuArray ($Config.Parameters.PSObject.Properties.Name)) }
+    $ConfigKey = if ($PathKey) { $PathKey } else { (Read-Menu -MenuArray ($Config.Paths.PSObject.Properties.Name)) }
 
     if (-not $ConfigKey) {
         Write-Host "Key not found."
         return
     }
 
-    $Path = $Config.Parameters.$ConfigKey
+    $Path = $Config.Paths.$ConfigKey
 
     code ($Path)
 }
--- Modules/Terminal/Terminal.psm1
+++ Modules/Terminal/Terminal.psm1
@@ -1,19 +1,19 @@
 [PSObject]$Config = (Use-Config).Data.Terminal
 
 function Terminal($PathKey) {
-    if (-not $Config.Parameters.PSObject.Properties.Length) {
+    if (-not $Config.Paths.PSObject.Properties.Length) {
         Write-Host "No keys found."
         return
     }
 
-    $ConfigKey = if ($PathKey) { $PathKey } else { (Read-Menu -MenuArray ($Config.Parameters.PSObject.Properties.Name)) }
+    $ConfigKey = if ($PathKey) { $PathKey } else { (Read-Menu -MenuArray ($Config.Paths.PSObject.Properties.Name)) }
 
     if (-not $ConfigKey) {
         Write-Host "Key not found."
         return
     }
 
-    $Path = $Config.Parameters.$ConfigKey
+    $Path = $Config.Paths.$ConfigKey
 
     Set-Location $Path
 }
